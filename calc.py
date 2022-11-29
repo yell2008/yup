c4585bf97e52caa83e757383db75f731893de99d
yell2008 <114228723+yell2008@users.noreply.github.com>
2022-11-29 20:30:18 +0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,11 +1,14 @@
-class Calculator:
-    def __init__(self):
-        pass
-    def sum(self,a,b):
-        return a+b
-    def minus(self,a,b):
-        return a-b
-    def div(self,a,b):
-        return a/b
-    def multi(self,a,b):
-        return a*b
+def simple_calculator(string):
+    try:
+        return eval(string, {}, {})
+    except:
+        return 'ERR'
+
+def main():
+    equation = input()
+    while equation:
+        print(simple_calculator(equation))
+        equation = input()
+
+if __name__ == '__main__':
+    main()
